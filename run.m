129ca5dfa4447cc9a21bac10d2a74f51ddc593aa
ethanknights <ethanknights@hotmail.co.uk>
2022-05-10 11:20:51 +0100

    tidy run.m

--- run.m
+++ run.m
@@ -1,34 +1,26 @@
+%% Purpose: Run original analysis routine that additionally writes RT files.
+%% ========================================================================
+
 clear
 
 CCIDList = dir('/imaging/camcan/cc700-rawdata/MRI/data/CC*'); CCIDList = {CCIDList.name}';
 
 for s = 1:length(CCIDList)
   
-ccid = CCIDList{s};
-%ccid = 'CC110033'
-
-
-raw_dir = '/imaging/camcan/cc700-rawdata'
-scored_dir = '/imaging/camcan/sandbox/ek03/fixCC700/SMT_RT/analysis_scripts/scored'; %mkdir(scored_dir)
-owflag = false;
-
-[R fname errmsg] = MRI_wrapper_script(ccid,raw_dir,scored_dir,owflag)
-
+  ccid = CCIDList{s};
+  
+  raw_dir = '/imaging/camcan/cc700-rawdata';
+  scored_dir = '/imaging/camcan/cc700-scored/MRI/release002/data'; %mkdir(scored_dir)
+  owflag = false;
+  
+  [R fname errmsg] = MRI_wrapper_script(ccid,raw_dir,scored_dir,owflag);
+  
 end
 
 return
-
 
 %% Notes
 %% ========================================================================
 %Failing subjects:
 %'CC410222'
-%This subject had no events file in 2019 BIDS either. Everyones event order
-%is the same... but time and RTs do vary
-
-
-%% Move New Data to Cam-CAN central space
-%% ========================================================================
-!mkdir /imaging/camcan/cc700-scored/MRI/release002
-!mv /imaging/camcan/sandbox/ek03/fixCC700/SMT_RT/* /imaging/camcan/cc700-scored/MRI/release002
-
+%This subject had no events file in 2019 BIDS either.
